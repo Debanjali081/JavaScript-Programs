f552ffd5ffe05ac526404412763bc7a0baadb673
debanjali081 <debanjali017@gmail.com>
2024-06-21 10:10:13 +0530

    1st commit

--- Loops/practice2.js
+++ Loops/practice2.js
@@ -2,7 +2,7 @@ let comNum = 13;
 
 let userNum = prompt("Guess a number  :");
 
-while (userNum !== comNum) {
-    prompt("You enterd wrong number please enter again :")
+while (userNum != comNum) {
+   userNum= prompt("You enterd wrong number please enter again :")
 }
 console.log("Congratulations you won the game !!!!")
